7c84bc9bd077459195542e84ab1a1bf855e8b091
marcusguttenplan <marcusguttenplan@gmail.com>
2016-10-08 18:56:42 -0700

    fix package.json

--- eyetrackr/app/js/main.js
+++ eyetrackr/app/js/main.js
@@ -1,1 +1,34 @@
+//
+// Allow access to webcam
+//
 
+// properties
+var content, webcam;
+
+function init() {
+    content = document.getElementById('content');
+
+    // adds listeners to activate and deactivate on iframe focus
+    window.addEventListener('focus', start, false);
+    window.addEventListener('blur', stop, false);
+
+    // instanciate our Webcam
+    webcam = new Webcam();
+    content.appendChild(webcam.domElement);
+}
+
+function start(e) {
+    e.preventDefault();
+    document.body.className = 'active';
+
+    webcam.start();
+}
+
+function stop(e) {
+    e.preventDefault();
+    document.body.className = '';
+
+    webcam.stop();
+}
+
+init();
